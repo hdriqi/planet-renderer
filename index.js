8158cd4122bc5476a6cc6c3bd4f896f190d18ac2
hdriqi <riqi.asdf@gmail.com>
2023-08-21 20:13:57 +0700

    add: api for planet only with transparent bg

--- index.js
+++ index.js
@@ -48,6 +48,26 @@ app.get('/planets/:id', async (req, res) => {
   res.send(buffer)
 })
 
+app.get('/planets-only/:id', async (req, res) => {
+  console.log(process.env.DB)
+  const data = await client.db(process.env.DB).collection('planets').findOne({ nft_id: parseInt(req.params.id) })
+  console.log(data)
+
+  const buffer = render.planetOnly({
+    seed: data.digest,
+    id: data.nft_id,
+    px: data.x,
+    py: data.y,
+    rts: data.rts,
+    mrts: data.mrts,
+    prts: data.prts,
+    arts: data.arts
+  })
+
+  res.contentType('image/png')
+  res.send(buffer)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- render.js
+++ render.js
@@ -3,9 +3,135 @@ const alea = require('alea')
 const { createNoise2D } = require('simplex-noise')
 const chroma = require('chroma-js')
 const ColorHash = require('color-hash').default
-const { GIFEncoder, quantize, applyPalette } = require('gifenc')
 
 const colorHash = new ColorHash()
+
+const generatePlanetOnly = (seed, mrts, prts, arts) => {
+  const canvas = createCanvas(256, 256)
+  const ctx = canvas.getContext('2d')
+
+  const SIZE = 256
+
+  const imageData = ctx.createImageData(SIZE, SIZE)
+
+  const prng = alea(seed)
+  const data = imageData.data
+  const noise2D = createNoise2D(prng)
+  let t = 1
+
+  const _amp = prng()
+
+  const colors = chroma.scale([colorHash.hex(mrts.toString()), colorHash.hex(prts.toString()), colorHash.hex(arts.toString())]).colors(5)
+  const bg = chroma('#000').rgba()
+
+  for (let x = 0; x < SIZE; x++) {
+    for (let y = 0; y < SIZE; y++) {
+      let i = x + y * SIZE
+      const dx = x - Math.floor(SIZE / 2)
+      const dy = y - Math.floor(SIZE / 2)
+      const distanceSqrd = dx ** 2 + dy ** 2
+      if (distanceSqrd < ((((SIZE / 2) - 4) ** 2))) {
+        const nx = x / SIZE - 0.5
+        const ny = y / SIZE - 0.5
+        let rgba = []
+        function fbm_noise(amplitudes, nx, ny) {
+          let sum = 0, sumOfAmplitudes = 0
+          for (let octave = 0; octave < amplitudes.length; octave++) {
+            let frequency = 1 << octave
+            sum += amplitudes[octave] * noise2D(nx * frequency, ny * frequency)
+            sumOfAmplitudes += amplitudes[octave]
+          }
+          return sum / sumOfAmplitudes;
+        }
+
+        let curAmp = Array(8).fill(1).map((v, i) => {
+          // best
+          // return i > 0 ? v * ((0.75) ** i) : v
+          return i > 0 ? v * ((_amp) ** i) : v
+        })
+
+        const e = fbm_noise(curAmp, nx, ny)
+        if (e > 0.5) {
+          rgba = chroma(colors[0]).rgba()
+          // rgba = chroma('white').rgba()
+        }
+        else if (e > 0.3) {
+          rgba = chroma(colors[0]).darken(1).rgba()
+          // rgba = chroma('white').darken(1).rgba()
+        }
+        else if (e > 0) {
+          rgba = chroma(colors[0]).darken(2).rgba()
+          // rgba = chroma('white').darken(2).rgba()
+        }
+        else {
+          rgba = chroma(colors[4]).brighten().rgba()
+        }
+        data[i * 4 + 0] = rgba[0] // R value
+        data[i * 4 + 1] = rgba[1]
+        data[i * 4 + 2] = rgba[2]
+        data[i * 4 + 3] = 255
+      }
+      else if (distanceSqrd <= (((SIZE / 2) ** 2))) {
+        data[i * 4 + 0] = 0
+        data[i * 4 + 1] = 0
+        data[i * 4 + 2] = 0
+        data[i * 4 + 3] = 255
+      }
+      else {
+        data[i * 4 + 0] = bg[0]
+        data[i * 4 + 1] = bg[1]
+        data[i * 4 + 2] = bg[2]
+        data[i * 4 + 3] = 0
+      }
+    }
+    t++
+  }
+  ctx.fillStyle = `rgba(0,0,0,0)`
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // put planet
+  ctx.putImageData(imageData, 0, 0)
+
+  function addlight(ctx, x, y) {
+    var grd = ctx.createRadialGradient(x, y, SIZE / 16, x, y, SIZE / 2)
+    grd.addColorStop(0, "transparent")
+    grd.addColorStop(1, "rgba(0,0,0,0.95)")
+    ctx.fillStyle = grd
+    ctx.fillRect(0, 0, SIZE, SIZE)
+  }
+
+  ctx.globalCompositeOperation = "multiply"
+
+  // put light
+  addlight(ctx, canvas.width / 2, canvas.height / 2)
+
+  // add transparent background
+  ctx.globalCompositeOperation = "source-in"
+
+  const planetImg = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const planetImgData = planetImg.data
+
+  for (let x = 0; x < SIZE; x++) {
+    for (let y = 0; y < SIZE; y++) {
+      let i = x + y * SIZE
+      const dx = x - Math.floor(SIZE / 2)
+      const dy = y - Math.floor(SIZE / 2)
+      const distanceSqrd = dx ** 2 + dy ** 2
+      if (!(distanceSqrd < ((((SIZE / 2) - 4) ** 2)))) {
+        planetImgData[i * 4 + 0] = 0
+        planetImgData[i * 4 + 1] = 0
+        planetImgData[i * 4 + 2] = 0
+        planetImgData[i * 4 + 3] = 0
+      }
+    }
+  }
+
+  ctx.putImageData(planetImg, 0, 0)
+
+  const buf = canvas.toBuffer('image/png', { compressionLevel: 3, filters: canvas.PNG_FILTER_NONE })
+
+  return buf
+}
 
 const generatePlanet = (seed, id, px, py, rts, mrts, prts, arts) => {
   const canvas = createCanvas(333, 480)
@@ -167,10 +293,16 @@ const main = ({
   return generatePlanet(seed, id, px, py, rts, mrts, prts, arts)
 }
 
+const planetOnly = ({
+  seed, id, px, py, rts, mrts, prts, arts
+}) => {
+  return generatePlanetOnly(seed, id, px, py, rts, mrts, prts, arts)
+}
+
 const gif = (seed) => {
   return seed
 }
 
 module.exports = {
-  main, gif
+  main, gif, planetOnly
 }
